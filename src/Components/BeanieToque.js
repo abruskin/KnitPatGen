d0232188bc6df9eb05732f492d82134e687281b7
abruskin <90646708+abruskin@users.noreply.github.com>
2022-12-29 17:20:36 -0600

    handling gauge works!

--- src/Components/BeanieToque.js
+++ src/Components/BeanieToque.js
@@ -1,8 +1,45 @@
-
+import { useState } from 'react'
+import FetchService from '../service/fetch'
 
 function BeanieToque() {
-//TODO make the backend, connect the backend, then set the gauge submit button to send data to the backend
+// then set the gauge submit button to send data to the backend
+const [swatchunits, setSwatchunits] = useState("inches")
+const [stitchesInSwatch, setStitchesInSwatch] = useState("")
+const [swatchWidth, setSwatchWidth] = useState("")
+const [rowsInSwatch, setRowsInSwatch] = useState("")
+const [swatchLength, setSwatchLength] = useState("")
 
+let handleGaugeSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        let res = await fetch("http://127.0.0.1:8000/gauge", {
+            method: "POST",
+            //adding content-type header fixed the cors error
+            //it tells the backend that we're dealing with json
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                swatchunits: swatchunits,
+                stitchesInSwatch: stitchesInSwatch,
+                swatchWidth: swatchWidth,
+                rowsInSwatch: rowsInSwatch,
+                swatchLength: swatchLength,
+            
+            })
+        });
+        console.log(res)
+        let resJson = await res.json();
+        if (res.status === 200) {
+            console.log('yay! sent to server')
+        } else {
+            console.log('oops')
+        }
+    } catch (err) {
+        console.log(err)
+    } 
+    
+} 
 
     return (
         <div>
@@ -20,7 +57,7 @@ function BeanieToque() {
 <option value="18.5">6-18 months ( 18.5 in / 47 cm)</option>
 <option value="20">18 months - 4 years ( 20 in / 51 cm)</option>
 <option value="21">4 years - adult small ( 21 in / 53.5 cm)</option>
-<option value="23"> adult medium/large (23 in / 58.5 cm</option>
+<option value="23"> adult medium/large (23 in / 58.5 cm)</option>
 </select>
 </label>
 
@@ -36,30 +73,29 @@ function BeanieToque() {
         <h2> About your swatch</h2>
         <p> You did a swatch, right? If you did, tell us about it. If you didn't, 
         go make one. We'll wait. </p>
-       <form>
-       <label>Stitches: 
+       <form className="swatchgauge">
+       
        <label>Measured in: 
-            <select> 
+            <select onChange={(e)=>setSwatchunits(e.target.value)}> 
             <option value="inches">in</option>
             <option value="centimeters">cm</option></select>
             </label> <br />
-
-            <input type="number" name="stitchesInSwatch" />
+            <label>Stitches: 
+            <input type="number" name="stitchesInSwatch" onChange={(e)=>setStitchesInSwatch(e.target.value)}/>
             </label>
             <label>Width: 
-            <input type="number" name="swatchWidth" />
+            <input type="number" name="swatchWidth" onChange={(e)=>setSwatchWidth(e.target.value)}/>
             </label> 
             
             <br />
             <label>Rows: 
-            <input type="number" name="rowsInSwatch" />
+            <input type="number" name="rowsInSwatch" onChange={(e)=>setRowsInSwatch(e.target.value)}/>
             </label>
             <label>Length: 
-            <input type="number" name="swatchLength" />
+            <input type="number" name="swatchLength" onChange={(e)=>setSwatchLength(e.target.value)}/>
             </label>   
             
-            
-            <button>Submit</button>
+            <button onClick={handleGaugeSubmit}>Calculate Gauge </button>
        </form>
         </div>
         
